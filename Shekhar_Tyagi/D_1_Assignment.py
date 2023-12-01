ab9d3397fd538c15a7d8a68555d22e85f8f1a646
shekhartyagi112 <shekhartyagi53@gmail.com>
2023-12-01 13:06:45 +0530

    D_1_Assignment

---
+++ Shekhar_Tyagi/D_1_Assignment.py
@@ -0,0 +1,17 @@
+#1). Python Program to add two integer values.
+
+x= 10
+y= 20
+z=x+y
+print(z)
+
+#2). Python Program to subtract two integer values.
+
+x=20
+y=10
+z=x-y
+
+print(z)
+
+#3). Python program to multiply two numbers.
+
