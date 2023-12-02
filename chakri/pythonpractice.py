fe6a41f1c356d42d688a439aa5cba6af8135b445
ChakriSiragam <chakrisiragam66@gmail.com>
2023-12-02 21:48:28 +0530

    pratice

---
+++ chakri/pythonpractice.py
@@ -0,0 +1,34 @@
+a = 100
+b = 300
+print(a+b)
+print(a*b)
+print(a/b)
+print(a-b)
+
+c = a/b
+print(type(c))
+
+a,b,c,d = 10,20,30,'chakri'
+print(a+b,a-c,c/a,c//a,a**2,d,type(d))
+
+e = True
+f = False
+print(e and f) #false
+print(e or f) #true
+print(not e) #false
+
+a,b=10,20
+print(a>b) #false
+print(a<b) #true
+print(a>=b) #false
+print(a<=b) #true
+print(a == b) #false
+print(a != b) #true
+
+
+
+
+
+
+
+
