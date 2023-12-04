2334d93d663e038829e5df088860b7061b51e51c
shekhartyagi112 <shekhartyagi53@gmail.com>
2023-12-04 07:57:45 +0530

    update

--- Shekhar_Tyagi/D_1_Assignment.py
+++ Shekhar_Tyagi/D_1_Assignment.py
@@ -64,8 +64,8 @@ print("Cube:", R2)
 #8). Python program to interchange values between variables.
 print("8). Python program to interchange values between variables.")
 
-a =  200      #input("enter the value1 : ")
-b =  300      #input("enter the value2 : ")
+a =   200 #int(input("enter the value1 : "))
+b =   300 #int(input("enter the value2 : "))
 a,b = b,a
 print("a:",a)
 print("b:",b)
@@ -124,6 +124,74 @@ R2 = 5**2-3**2
 print("(a^2-b^2):",R1)
 print("(a^2-b^2):",R2)
 
-# #13). Python program to solve the given math formula.
+#13). Python program to solve the given math formula.
 # Formula : (a + b)3 = a3 + 3ab(a+b) + b3
+print("13). Python program to solve the given math formula.")
+
+a = 5
+b = 6
+
+# R1 = a^3 + 3*a*b(a+b) + b^3
+R1 = 5**3 + 3*5*6*(5+6) +6**3
+print("(a+b)^3:", R1)
+# R2 = (a+b)^3
+R2 = (5+6)**3
+print("(a+b)^3:",R2)
+
+# #14). Python program to solve the given math formula.
+# Formula : (a – b)3 = a3 – 3a2b + 3ab2 – b3
+print("14). Python program to solve the given math formula.")
+
+a = 6
+b = 4
+# R1 = a^3 -3*a^2*b + 3*a*b^2 -b^3
+R1 = 6**3 - 3*6**2*4 + 3*6*4**2 -4**3
+print("(a-b)^3:", R1)
+# R2 = (a-b)^3
+R2 = (6-4)**3
+print("(a-b)^3:",R2)
+
+#15). Python program to calculate the area of the square.
+# Formula : area = a*a
+print("15). Python program to calculate the area of the square.")
+
+a = 10 #int(input("enter the value of the square side: "))
+# R1 = a**2
+R1 = a**2
+print("(Area of square:",R1)
+
+# 16). Python program to calculate the area of a circle.
+# Formula = PI*R^2
+# R = Radius
+# PI = 3.14    # value of Pi
+print("16). Python program to calculate the area of a circle.")
+
+R = 9 #int(input("Enter the value of R:"))
+Pi = 3.14
+Area = Pi * R**2
+print("(Area of circle:)", Area)
+
+# 17). Python program to calculate the area of a cube.
+# Formula = 6*a*a
+print("17). Python program to calculate the area of a cube.")
+
+a = 7 #int(input("Enter the value of side:"))
+R = 6 *a**2
+print("(Area of cube:)", R)
+
+# 18). Python program to calculate the area of the cylinder.
+# Formula = 2*PI*r*h + 2*PI*r*r
+print("18). Python program to calculate the area of the cylinder.")
+#r = radius of cylinder
+#h = Hight of cylinder
+
+r =  5  #int(input("Enter the value of radius of cylinder:"))
+h =  10 #int(input("Enter the value of hight of cylinder:"))
+Pi = 3.14   # value of Pi
+Area = 2*Pi*r*h + 2*Pi*r**2
+print("(Area of cylinder:)",Area)
+
+# 19). Python program to check whether the given number is an Armstrong number or not.
+# Example: 153 = 1*1*1 + 5*5*5 + 3*3*3
+print("19). Python program to check whether the given number is an Armstrong number or not.")
 
