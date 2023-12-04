277b19892c8bff8257d52156dc16c4afa5102679
ChakriSiragam <chakrisiragam66@gmail.com>
2023-12-04 12:28:47 +0530

    basic progrmmes

---
+++ chakri/basicprogramming-chakri.py
@@ -0,0 +1,38 @@
+#1). Python Program to add two integer values.
+
+a,b = 10,20
+c=a+b
+print(c)
+#2). Python Program to subtract two integer values.
+
+a,b = 10,20
+c=a-b
+print(c)
+#3). Python program to multiply two numbers.
+
+e,f = 10,4
+print(e*f)
+
+#4). Python program to repeat a given string 5 times.
+str1 = "Chakri"
+print(str1*5)
+
+#5). Python program to get the Average of given numbers.
+a,b,c = 40,50,80
+d = a+b+c
+print(d/3)
+#5a
+a,b,c = 40,50,80
+d = a+b+c
+print(d//3)
+
+#7). Python program to print the square and cube of a given number.
+num1 = 9
+print("square = ", num1**2)
+print("cube = ", num1**3)
+
+#8). Python program to interchange values between variables.
+a,b = 30,40
+b,a = a,b
+print(a,b)
+
