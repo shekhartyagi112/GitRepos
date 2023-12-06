9aa0bf128bfac72434d2f0354d747c69a1b0f51e
unknown <rvankita@gmail.com>
2023-12-06 12:34:46 +0530

    New file added

---
+++ Ankita Verma/AV1/Type_Conversion.py
@@ -0,0 +1,29 @@
+
+"""1.  Numbers
+    a).  Integer
+    b).  Float
+    c). Complex Number
+
+2.  Sequencial data type
+    d). String
+    e). Tuple
+    f). List
+
+3. Dictionary
+
+4.Set
+5.Boolean"""
+
+#Interger Type Conversion to Float""
+"""Number=40
+Num2=float(Number)
+print(Num2,type(Num2))"""""
+
+#Integer to String
+Number= 98765
+Str1=str(Number)
+print(Str1,type(Str1),Str1[3])
+
+
+
+
