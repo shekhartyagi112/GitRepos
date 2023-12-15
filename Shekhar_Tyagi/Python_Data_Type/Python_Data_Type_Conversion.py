aedba5d340781eb455e1e2f138b5068f592d5229
shekhartyagi112 <shekhartyagi53@gmail.com>
2023-12-15 13:42:06 +0530

    If Else Condition

--- Shekhar_Tyagi/Python_Data_Type/Python_Data_Type_Conversion.py
+++ Shekhar_Tyagi/Python_Data_Type/Python_Data_Type_Conversion.py
@@ -404,30 +404,18 @@ print(bool_1, type(bool_1))
 
 
 
-
-
-
-
-
-
-
-
-
-
-"""
 even_sum = 0
 odd_sum = 0
 
-for i in range(1,21):
+for i in range(1,5):
     if i%2==0:
         even_sum= even_sum+i
     else:
         odd_sum= odd_sum +i
-    print("even_sum :", i,"," , even_sum)
-    print("odd_sum :",i, "," ,odd_sum)
+print("even_sum :", i,"," , even_sum)
+print("odd_sum :",i, "," ,odd_sum)
 
 
-"""
 
 
 
