44864976e8c852864f99866d06a8f18dffd0b8c0
tusharaher3131 <tusharaher3131@gmail.com>
2023-12-15 14:49:09 +0530

    up

---
+++ Tushar/if_else_practice_7Dec.py
@@ -0,0 +1,49 @@
+#syntax of if else:
+
+"""
+if condition:
+   code
+else:
+    code
+"""
+
+
+##programme for check number equal or not
+'''
+num1=100
+num2=100
+if num1==num2:
+    print("numbers are matching")
+else:
+    print("numbers are not matching")
+'''
+''' 
+num1=int(input("please enter number:"))
+num2=int(input("please enter number:"))
+
+if num1==num2:
+    print("numbers are matching",num1,num2)
+else:
+    print("numbers are not matching",num1,num2)
+'''
+
+# for identation error select code and press cntrl + alt +i
+
+###write programme for to check even and odd no
+'''
+num1=51
+if num1%2==0:
+    print("This is even no",num1)
+else:
+    print("This is odd no",num1)
+'''
+'''
+user_input=int(input("please enter no"))
+if user_input%2==0:
+    print("This is even number",user_input)
+else:
+    print("This is Odd number",user_input)
+'''
+
+
+
